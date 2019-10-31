0c09b99537270edb58502df18aaf2da0e0a8cae6
upinetree <upinetree@gmail.com>
2019-10-31 15:54:26 +0900

    Add a missing namespace

--- lib/depdump/dependency_graph.rb
+++ lib/depdump/dependency_graph.rb
@@ -1,18 +1,20 @@
-class DependencyGraph
-  attr_reader :nodes, :edges
+class Depdump
+  class DependencyGraph
+    attr_reader :nodes, :edges
 
-  def initialize(tree)
-    @nodes = {}
-    @edges = Set.new
-    build(tree)
-  end
+    def initialize(tree)
+      @nodes = {}
+      @edges = Set.new
+      build(tree)
+    end
 
-  def build(tree)
-    tree.each_node do |node|
-      @nodes[node.key] ||= node.namespaces
-      node.relations.each do |r|
-        referenced_namespaces = r.resolve(tree)
-        @edges << { from: node.namespaces, to: referenced_namespaces } if referenced_namespaces
+    def build(tree)
+      tree.each_node do |node|
+        @nodes[node.key] ||= node.namespaces
+        node.relations.each do |r|
+          referenced_namespaces = r.resolve(tree)
+          @edges << { from: node.namespaces, to: referenced_namespaces } if referenced_namespaces
+        end
       end
     end
   end
